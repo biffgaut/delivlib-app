fa87aad054a8d1259d5b6a8dea534819dba419c2
biffgaut <78155736+biffgaut@users.noreply.github.com>
2022-06-10 10:33:45 -0400

    Add Test, buildspec

--- test/delivlib-app.test.ts
+++ test/delivlib-app.test.ts
@@ -1,17 +1,17 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as DelivlibApp from '../lib/delivlib-app-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as DelivlibApp from '../lib/delivlib-app-stack';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/delivlib-app-stack.ts
 test('SQS Queue Created', () => {
-//   const app = new cdk.App();
+  const app = new cdk.App();
 //     // WHEN
-//   const stack = new DelivlibApp.DelivlibAppStack(app, 'MyTestStack');
+  const stack = new DelivlibApp.DelivlibAppStack(app, 'MyTestStack');
 //     // THEN
-//   const template = Template.fromStack(stack);
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  template.hasResourceProperties('AWS::SQS::Queue', {
+    VisibilityTimeout: 300
+  });
 });
